17170ca4f685396d7312e842d3d81960b9a777bc
Your Name <you@example.com>
2023-05-04 14:42:02 +0600

    first own section added

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import ChefCard from './ChefCard';
+import SiteInfo from './SiteInfo';
 
 const Home = () => {
     const [data, setData] = useState([])
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('http://localhost:3000/data')
-        .then(res => res.json())
-        .then(data => setData(data))
-    },[])
+            .then(res => res.json())
+            .then(data => setData(data))
+    }, [])
     return (
-        <div className='grid grid-cols-3 p-16 gap-4'>
-            {
-                data.map(info => <ChefCard
-                key={info.id}
-                data={info}
-                ></ChefCard>)
-            }
-            
+        <div>
+            <SiteInfo></SiteInfo>
+            <div className='grid grid-cols-3 p-16 gap-4'>
+                {
+                    data.map(info => <ChefCard
+                        key={info.id}
+                        data={info}
+                    ></ChefCard>)
+                }
+
+            </div>
         </div>
     );
 };
---
+++ src/components/SiteInfo.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import { FaUserAlt,FaScroll,FaAddressBook } from "react-icons/fa";
+
+const SiteInfo = () => {
+    return (
+        <div className='grid grid-cols-3 gap-2 px-20 mx-auto mt-16'>
+            <div className='p-8 border border-lime-600 bg-lime-50 text-center rounded'>
+                <FaUserAlt className='text-[35px] text-lime-600 mx-auto'></FaUserAlt>
+                <h2 className='text-[20px] font-medium text-zinc-900'>Subscriber</h2>
+                <p className='text-[50px] font-bold text-zinc-900'>30k+</p>
+            </div>
+            <div className='p-8 border border-lime-600 bg-lime-50 text-center rounded'>
+                <FaScroll className='text-[35px] text-lime-600 mx-auto'></FaScroll>
+                <h2 className='text-[20px] font-medium text-zinc-900'>Daily Visitor</h2>
+                <p className='text-[50px] font-bold text-zinc-900'>50k+</p>
+            </div>
+            <div className='p-8 border border-lime-600 bg-lime-50 text-center rounded'>
+                <FaAddressBook className='text-[35px] text-lime-600 mx-auto'></FaAddressBook>
+                <h2 className='text-[20px] font-medium text-zinc-900'>Daily book</h2>
+                <p className='text-[50px] font-bold text-zinc-900'>50/60</p>
+            </div>
+        </div>
+    );
+};
+
+export default SiteInfo;
