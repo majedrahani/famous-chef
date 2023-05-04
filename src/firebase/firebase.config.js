f47865304695328d8126a4eca9a299d4d7b7d1bf
Your Name <you@example.com>
2023-05-04 16:29:17 +0600

    hide firebase config

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,18 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import { Input } from "postcss";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('eviroment variable', import.meta.env.VITE_apiKey);
 const firebaseConfig = {
-  apiKey: "AIzaSyDLTY0qcFVCyo2o3Q6-9OfwDyXAYbxih2o",
-  authDomain: "famous-chef.firebaseapp.com",
-  projectId: "famous-chef",
-  storageBucket: "famous-chef.appspot.com",
-  messagingSenderId: "477367992813",
-  appId: "1:477367992813:web:89dc0c67a170f17601a7e4"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
